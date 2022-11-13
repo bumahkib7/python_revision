ed7c30f73708614256dbb572ad94fb1f4bcc7267
Bukhari bin mahmoud Kibuka <93518580+bumahkib7@users.noreply.github.com>
2022-11-13 11:51:22 +0100

    initial commit

---
+++ Excersices/ex34.py
@@ -0,0 +1,16 @@
+animals = ['bear', 'tiger', 'penguin', 'zebra']
+bear = animals[0]
+
+print(f"the animal is : {bear}".capitalize())
+
+animals2 = ['bear', 'python3.6', 'peacock', 'kangaroo', 'whale', 'platypus']
+
+print(animals2[1])#python
+print(animals2[2])#peacock
+print(animals2[0])
+print(animals2[3])
+print(animals2[4])
+print(animals2[2])
+print(animals2[5])
+print(animals2[4])
+
